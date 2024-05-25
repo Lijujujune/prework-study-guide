1df722ba1c951ffa066628c77c248db26bf62ac3
Lijujujune <yejunli@YEJUNs-MacBook-Air.local>
2024-05-25 22:25:26 +1000

    Add JaveScripts

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+
+/*
+const topic = "HTML";
+
+ if (topic === 'HTML') {
+    console.log("let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JaveScript!")
+} else {
+    console.log('Please try again!');
+}
+*/
